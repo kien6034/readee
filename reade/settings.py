2e18403b6e1e1830162874e94f6d9738ae07cba1
kien6034 <trinhtrungkientb99@gmail.com>
2020-09-27 22:41:08 +0700

    environment variable

--- reade/settings.py
+++ reade/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^fgp8rc$pf^xr1sb^&=20*r-)ar6#g%w)(00p4joq*+0tq12(9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['readee.herokuapp.com']
 
 
 # Application definition
